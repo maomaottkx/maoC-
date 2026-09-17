21fadfe4b4f75d02c5c02d594e9a79aa35ff7381
mao <369172001@qq.com>
2026-09-17 18:48:56 +0800

    scm-history-item:e%3A%5CCppProject?%7B%22repositoryId%22%3A%22scm0%22%2C%22historyItemId%22%3A%2231157b3a23bb95ea7af7069bb366706417d9ef1c%22%2C%22historyItemDisplayId%22%3A%2231157b3%22%7D

---
+++ exam1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    int x;
+    vector<int> v;
+    while (true) {
+        cout << "请输入整数：";
+        cin >> x;
+        v.push_back(x);
+    }
+    sort(v.begin(), v.end());
+    cout << "数据从小到大为：";
+    for (int i = 0;i < v.size();i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
